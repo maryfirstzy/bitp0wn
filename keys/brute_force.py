c1796ae636f83decd1d6aa4947fb97cb1b572d97
maryfirstzy <maryfirst865@gmail.com>
2026-07-15 11:57:40 -0400

    Update brute_force.py

--- keys/brute_force.py
+++ keys/brute_force.py
@@ -11,8 +11,8 @@ k = random.randint(0, 2**nbits - 2 ** (nbits // 2))
 q = fast_multiply(G, k)[0]
 print("SEARCH - {0}".format(k))
 
-for n in range(2**nbits - 2 ** (nbits / 2)):
-    candidate = fast_multiply(G, n)
+for n in range(2**nbits - 2 ** (nbits // 2)):
+ candidate = fast_multiply(G, n)
     if candidate[0] == q:
         print("FOUND  - {0}".format(n))
         break
